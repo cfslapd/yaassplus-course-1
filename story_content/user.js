90d97ae9c213341f0c2089f1a6c65fa7e9f51e94
W Trinidad <cfslapdtraining@gmail.com>
2022-02-28 15:50:37 +0800

    first

---
+++ story_content/user.js
@@ -0,0 +1,52 @@
+function ExecuteScript(strId)
+{
+  switch (strId)
+  {
+      case "5hZFxPKrWTB":
+        Script1();
+        break;
+      case "63oxR8QgiFv":
+        Script2();
+        break;
+  }
+}
+
+function Script1()
+{
+  
+               var field3 =1;
+ 
+                $.ajax({
+                url: "https://docs.google.com/forms/d/1N9UGxJBvBSglj3ozn7mQdR_64EZtRwFDNlEgnRpQ1Ow/formResponse",
+                data: {"entry.897097011": field3}.submit(),
+                type: "POST",
+                dataType: "xml",
+                statusCode: {
+                    0: function() {
+                        //Success message
+                    },
+                    200: function() {
+                        //Success Message
+                    }
+                }
+            });
+
+         $(document).ready(function(){
+                $('#form').submit(function() {
+                    postToGoogle();
+                    return false;
+                });
+            });
+
+}
+
+function Script2()
+{
+              $(document).ready(function(){
+                $('#form').submit(function() {
+                    postToGoogle();
+                    return false;
+                });
+            });
+}
+
